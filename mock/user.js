d4c47b726348c1154e6b14e2691c589f82a31e32
liguantong <codertg@163.com>
2023-06-19 23:12:19 +0800

    user接口实现

---
+++ mock/user.js
@@ -0,0 +1,40 @@
+const Mock = require("mockjs");
+
+const Random = Mock.Random;
+
+module.exports = [
+  {
+    url: "/api/user/info",
+    method: "get",
+    response() {
+      return {
+        error: 0,
+        data: {
+          username: Random.cname,
+          nickname: Random.cname,
+        },
+      };
+    },
+  },
+  {
+    url: "/api/user/register",
+    method: "post",
+    response() {
+      return {
+        error: 0,
+      };
+    },
+  },
+  {
+    url: "/api/user/login",
+    method: "post",
+    response() {
+      return {
+        error: 0,
+        data: {
+          token: Random.word(20),
+        },
+      };
+    },
+  },
+];
