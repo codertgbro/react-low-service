d66e9bc18a311be6605f2bdbd05efd77af45549f
liguantong <liguantong@tuzhanai.com>
2023-08-08 17:26:03 +0800

    update

--- mock/data/getComponentList.js
+++ mock/data/getComponentList.js
@@ -14,18 +14,24 @@ function getComponentList() {
       fe_id: 'c1', // 注意，由于统计页，左侧和中间需要数据完全一直，所以要写死 fe_id ，不能用 Random.id()
       type: 'questionTitle', // 组件类型，不能重复，前后端统一好
       title: '标题',
-      props: { title: '个人信息调研', level: 1, isCenter: false }
+      isVisable: true,
+      isLock: false,
+      props: { text: '个人信息调研', level: 1, isCenter: false }
     },
     {
       fe_id: 'c2',
       type: 'questionInput',
       title: '输入框1',
+      isVisable: true,
+      isLock: false,
       props: { title: '你的姓名', placeholder: '请输入姓名...' }
     },
     {
       fe_id: 'c3',
       type: 'questionInput',
       title: '输入框1',
+      isVisable: true,
+      isLock: false,
       props: { title: '你的电话', placeholder: '请输入电话...' }
     },
   ]
